590ceb6a65252e256799c92ef17698154516f1c0
Anastacia <tashysam6@gmail.com>
2022-03-15 12:21:38 +0300

    add file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 /**
  * main - Entry point
- * 
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char *sh ="Holberton";
-      while (*sh)
+	char *sh = "Holberton";
 
-      {
-      _putchar(*sh);
-      sh++;
-      }
-    _putchar('\n');
-    return (0);
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
