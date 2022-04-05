abd916a52a3c18a6d844d44ffe024b9f18bc09f8
Anastacia <tashysam6@gmail.com>
2022-04-05 23:07:43 +0300

    update me

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,20 +1,56 @@
-#include <stdio.h>
 #include "main.h"
+/**
+* argstostr - a function that concatenates all the arguments of your program
+*@ac: count of args passed to the function
+*@av:array of arguments
+*
+*Return: pointer to the new string
+*/
+
+char *argstostr(int ac, char **av)
+{
+		char *new_string = NULL;
+		int k = 0, i = ac, j, sum = 0, temp = 0;
+
+		if (ac == 0 || av == NULL)
+			return (NULL);
+
+		while (ac--)
+			sum += (len(av[ac]) + 1);
+		new_string = (char *) malloc(sum + 1);
+
+		if (new_string != NULL)
+		{
+			while (k < i)
+			{
+				for (j = 0; av[k][j] != '\0'; j++)
+					new_string[j + temp] = av[k][j];
+				new_string[temp + j] = '\n';
+				temp += (j + 1);
+				k++;
+			}
+			new_string[temp] = '\0';
+		}
+		else
+		{
+			return (NULL);
+		}
+		return (new_string);
+}
 
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
-
-int _strlen(char *s)
+*len - returns length of str
+*@str: string counted
+*Return: returns the length
+*/
+int len(char *str)
 {
-	int l = 0;
+		int len = 0;
 
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
+		if (str != NULL)
+		{
+			while (str[len])
+				len++;
+		}
+	return (len);
 }
